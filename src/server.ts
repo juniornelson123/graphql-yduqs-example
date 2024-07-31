a64e550743e250be937e1b861a6f079789bf154d
Nelson junior <junior123nelson@gmail.com>
2024-07-31 16:48:27 -0300

    servidor conectando

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import mongoose, { ConnectOptions } from 'mongoose';
 const app = express() as any;
 
 // Conectar ao MongoDB no Docker
-mongoose.connect(process.env.MONGO_URL!, {
+mongoose.connect("mongodb+srv://junior123nelson:glylWqACgyBBlUrN@cluster0.3ju5ces.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 } as ConnectOptions);
